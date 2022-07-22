43715964a68203a4646147c081c759be5798e90f
HramkovaJulia <ylechka379@mail.ru>
2022-07-22 15:22:33 +0600

    Исправила ошибки 1 доработка

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Artyom Prima on 13.07.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+
+    override func draw(_ rect: CGRect) {
+    }
+
+    @IBOutlet weak var text: UITextView!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var dateOfBith: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var image: UIImageView!
+}
+
---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Artyom Prima on 13.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
