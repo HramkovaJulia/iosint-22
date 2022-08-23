724790766d5712855a96a6ae5909ec68c693686e
HramkovaJulia <ylechka379@mail.ru>
2022-08-23 15:34:06 +0600

    Доработка TextField

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -18,7 +18,7 @@ class ProfileHeaderView: UIView {
         imageView.layer.cornerRadius = 80
         imageView.clipsToBounds = true
         imageView.layer.borderColor = UIColor.white.cgColor // цвет рамки
-        imageView.layer.borderWidth = 5 // толщина рамки
+        imageView.layer.borderWidth = 3 // толщина рамки
         return imageView
     }()
     
@@ -30,15 +30,24 @@ class ProfileHeaderView: UIView {
         return nameLabel
     }()
     
+    private lazy var statusLabel: UILabel = {
+        let statusLabel = UILabel()
+        statusLabel.text = ""
+        statusLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        statusLabel.textAlignment = .left
+//        statusLabel.backgroundColor = .red
+        return statusLabel
+    }()
+    
     private lazy var button: UIButton = {
         
         let button = UIButton()
         button.backgroundColor = .blue
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 4
         button.setTitle("Show my status", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        button.layer.shadowRadius = 10
+        button.layer.shadowRadius = 4
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOffset = CGSize(width: -4, height: 4)
         button.layer.shadowOpacity = 0.7
@@ -50,11 +59,16 @@ class ProfileHeaderView: UIView {
     
     private lazy var textField: UITextField = {
         let textField = UITextField()
-        //        textField.backgroundColor = .blue
         textField.placeholder = "Enter your status ..."
+        textField.textColor = .lightGray
+        textField.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        textField.clearButtonMode = .whileEditing // Кнопка «Очистить» при редактировании
+         textField.clearButtonMode = .always // Всегда показывать кнопку очистки
+        
         textField.returnKeyType = .done
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
         textField.becomeFirstResponder()
-        //        textField.delegate = self
+        
         return textField
         
     }()
@@ -73,7 +87,7 @@ class ProfileHeaderView: UIView {
         if Double(textField.text!) != nil {
             print("Name format is wrong")
         } else {
-            nameLabel.text = textField.text
+            statusLabel.text = textField.text
             textField.text = ""
         }
     }
@@ -82,29 +96,32 @@ class ProfileHeaderView: UIView {
         self.backgroundColor = #colorLiteral(red: 0.8196074367, green: 0.8196083307, blue: 0.8411096334, alpha: 1)
         self.addSubview(self.imageView)
         self.addSubview(self.nameLabel)
+        self.addSubview(self.statusLabel)
         self.addSubview(self.button)
         self.addSubview(self.textField)
         
-        self.imageView.frame = CGRect(x: 10,
-                                      y: 10,
+        self.imageView.frame = CGRect(x: 16,
+                                      y: 16,
                                       width: 160,
                                       height: 160)
         self.nameLabel.frame = CGRect(x: 177,
                                       y: 27,
-                                      width: 200,
+                                      width: 198,
                                       height: 21)
-        self.button.frame = CGRect(x: 10,
+        self.statusLabel.frame = CGRect(x: 180,
+                                        y: 80,
+                                        width: 198,
+                                        height: 40)
+        self.button.frame = CGRect(x: 16,
                                    y: 186,
-                                   width: 370,
+                                   width: 358, // расстояние слева справа по 16
                                    height: 50)
-        self.textField.frame = CGRect(x: 177,
+        self.textField.frame = CGRect(x: 180,
                                       y: 130,
-                                      width: 200,
+                                      width: 198,
                                       height: 40)
         
     }
     
 }
-
-
 
